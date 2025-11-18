a5ff64360264e8f0e4da3799acc5b948611168ff
Cezar <ioacaracezar@gmail.com>
2025-11-18 10:26:06 +0200

    merge pbn

--- OrderManagementSystemLean/src/main/java/com/example/OrderManagementSystem/model/ContractType.java
+++ OrderManagementSystemLean/src/main/java/com/example/OrderManagementSystem/model/ContractType.java
@@ -3,7 +3,7 @@ package com.example.OrderManagementSystem.model;
 public class ContractType implements Identifiable {
     private Long id;
     private String name;
-    private String type; // As in the UML: "Customer" or "Seller"
+    private String type;
 
     public ContractType() {}
 
